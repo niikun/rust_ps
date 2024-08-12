787aa8bc8d0095aafc92ca5e71e1d1968ba8cc8a
niikun <123465566+niikun@users.noreply.github.com>
2024-08-12 11:27:23 +0000

    proceed_height

---
+++ proceed_height/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let proceed = false;
+    if proceed{
+        println!("Proceeding");
+    }else{
+        println!("Not proceeding");
+    }
+
+    let height = 180;
+    if height > 180 {
+        println!("tall");
+    } else if height > 170 {
+        println!("average");
+    } else {
+        println!("short");
+    }
+}
