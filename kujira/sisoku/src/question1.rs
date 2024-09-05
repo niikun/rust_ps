7a86b8cce4581bdd8ef6dae7fc67d8d476445957
niikun <123465566+niikun@users.noreply.github.com>
2024-09-05 05:26:46 +0000

    kujira

---
+++ kujira/sisoku/src/question1.rs
@@ -0,0 +1,12 @@
+pub fn run(){
+    let distance:u32 = 384_400;
+    // per hour
+    let bicycle:u32 = 80;
+    let sinkansen :u32 = 300;
+
+    let hours_bicycle:u32 = distance / bicycle;
+    let days_bicycl
+    println!("{}",hours_bicycle);
+
+
+}
